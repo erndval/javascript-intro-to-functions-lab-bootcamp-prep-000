282e68ddaf35f14740b163b04c7d29266cfda45c
Ernie Val <erndval@gmail.com>
2019-02-22 03:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 
-function sayHitoGrandma(string){
+function sayHiToGrandma(string){
   if (string.tolowerCase === string){
   return 'I can\'t hear you!'
   }
